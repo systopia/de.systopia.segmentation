7b1271d55fe45a3c5024f4c1168ee853a340e030
Patrick Figel <pfigel@greenpeace.org>
2018-06-26 13:16:45 +0200

    Update logging schema

--- CRM/Segmentation/Upgrader.php
+++ CRM/Segmentation/Upgrader.php
@@ -10,18 +10,24 @@ class CRM_Segmentation_Upgrader extends CRM_Segmentation_Upgrader_Base {
   public function upgrade_0900() {
     $this->ctx->log->info('Updating segmentation schema to 0.9.0');
     $this->executeSqlFile('sql/create_activity_contact_segmentation.sql');
+    $logging = new CRM_Logging_Schema();
+    $logging->fixSchemaDifferences();
     return TRUE;
   }
 
   public function upgrade_0910() {
     $this->ctx->log->info('Updating segmentation schema to 0.9.1');
     $this->executeSqlFile('sql/add_bundle_and_text_block.sql');
+    $logging = new CRM_Logging_Schema();
+    $logging->fixSchemaDifferences();
     return TRUE;
   }
 
   public function upgrade_0920() {
     $this->ctx->log->info('Updating segmentation schema to 0.9.2');
     $this->executeSqlFile('sql/create_segmentation_exclude.sql');
+    $logging = new CRM_Logging_Schema();
+    $logging->fixSchemaDifferences();
     return TRUE;
   }
 
